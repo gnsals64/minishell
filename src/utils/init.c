37456b2f12cec5694916b5a548b25d09f8610a7f
ppparkta <pppppark2@gmail.com>
2023-03-22 20:39:15 +0900

    chore minishell.c 제외한 파일 정리

---
+++ src/utils/init.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 14:30:36 by marvin            #+#    #+#             */
+/*   Updated: 2023/02/27 14:30:36 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+t_data	*ft_init(char **env)
+{
+	t_data	*data;
+	int		i;
+
+	i = 0;
+	data = (t_data *)malloc(sizeof(t_data));
+	if (!data)
+		return (NULL);
+	ft_memset(data, 0, sizeof(data));
+	while (env[i] != NULL)
+	{
+		if (ft_env_creat(data, env[i]) == -1)
+			exit(1);
+		i++;
+	}
+	g_global.env_lst = data->env_head;
+	signal(SIGINT, handler);
+	return (data);
+}
